f37ee834363d2f613cb3b50b6c2a1537a12b2342
Nicolas Risso <risso.nicolas@usp.br>
2023-07-23 12:59:05 -0300

    FIltro funciona

--- GeradorDeRelatorios.java
+++ GeradorDeRelatorios.java
@@ -12,7 +12,10 @@ public class GeradorDeRelatorios {
     public static final int FORMATO_ITALICO = 0b0010;
     public static final int FORMATO_NEGRITO_ITALICO = 0b0011;
 
-    public void gerarRelatorio(Produto[] produtos, String algoritmo, String criterio) {
+    public static final String FILTRO_ESTOQUE_MENOR_OU_IQUAL_A = "estoque_menor_igual";
+	public static final String FILTRO_CATEGORIA_IGUAL_A = "categoria_igual";
+
+    public void gerarRelatorio(Produto[] produtos, String algoritmo, String criterio, String filtro, String parametro) {
         
         Ordenar ordenar = new Ordenar();
         produtos = ordenar.ordena(produtos, algoritmo, criterio);
@@ -25,6 +28,14 @@ public class GeradorDeRelatorios {
             writer.println("<tr><th>ID</th><th>DESCRIÇÃO</th><th>CATEGORIA</th><th>QUANTIDADE_ESTOQUE</th><th>PREÇO</th><th>NEGRITO</th><th>ITALICO</th><th>COR</th></tr>");
 
             for (Produto p : produtos) {
+                //filtro
+                if (filtro.equals(FILTRO_ESTOQUE_MENOR_OU_IQUAL_A)){
+                    if (p.getQtdEstoque() > Integer.parseInt(parametro)) continue;
+                }
+                if (filtro.equals(FILTRO_CATEGORIA_IGUAL_A)){
+                    if (!(p.getCategoria().equals(parametro))) continue;
+                }
+
                 writer.println("<tr>");
                 writer.println("<td>" + p.getId() + "</td>");
                 writer.println("<td>" + p.getDescricao() + "</td>");
@@ -68,14 +79,13 @@ public class GeradorDeRelatorios {
 		
 		String [] opcoes_formatacao = new String[2];
 		opcoes_formatacao[0] = args[4];
-        if (args.length > 4) opcoes_formatacao[1] = "";
+        if (args.length <= 5) opcoes_formatacao[1] = "";
         else opcoes_formatacao[1] = args[5];
 		
 
         boolean negrito = false;
         boolean italico = false;
 
-        //NN TA FUNFANDO DIREITO
         if (opcoes_formatacao[0].equals("negrito") || opcoes_formatacao[1].equals("negrito")) negrito = true;
         if (opcoes_formatacao[0].equals("italico") || opcoes_formatacao[1].equals("italico")) italico = true;
 
@@ -87,6 +97,6 @@ public class GeradorDeRelatorios {
         Produto[] produtos = csvReader.readCSV("produtos.csv");
 
         GeradorDeRelatorios gerador = new GeradorDeRelatorios();
-        gerador.gerarRelatorio(produtos, opcao_algoritmo, opcao_criterio_ord);
+        gerador.gerarRelatorio(produtos, opcao_algoritmo, opcao_criterio_ord, opcao_criterio_filtro, opcao_parametro_filtro);
     }
 }
